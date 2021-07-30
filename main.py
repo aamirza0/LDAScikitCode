9b799dd285e19a217ae50822e9865ddd4e00cae4
Aadam Mirza <87286325+aamirza0@users.noreply.github.com>
2021-07-30 18:24:08 +0000

    removed some excess code

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 #import the correct data. To keep the results as consistent as possible between manual implementation and Scikit implementation, mouseID (target values, 0 being no exercise and 1 being exercise), will be a large array and not a read-in from a csv, unfortunately. The sequence of zeroes and ones to plug in to the mouseID array should be in the LDACodes.txt file. 
-print('bruh')
 data = pd.read_csv('LDAWeek9RawData.csv')
 
 #Copy/Paste the sequence of ones and zeroes from the LDACodes files that corresponnd to the week being used for analysis (in other words, the code corresponding to the name of the csv being read in line 5) 
@@ -15,7 +14,7 @@ mouseTrain, mouseTest, classTrain, classTest = train_test_split(data,mouseID, te
 #With the creation of the training data complete, create and fit an LDA object to it. 
 mouseLDA = LinearDiscriminantAnalysis()
 mouseLDA.fit(mouseTrain,classTrain)
-#Now that that's done, evaluate the LDA model using the test data from earlier, and compare results to determine model accuracy. 
+#Now that that's done, evaluate the LDA model using the test data from earlier, and compare results to determine model accuracy. The accuracy score method does this by taking the test data (classTest) and comparing it to the output from the predictions made, which are stored in the classPredict variable. 
 classPredict = mouseLDA.predict(mouseTest)
 print('Accuracy of Model: ', accuracy_score(classTest,classPredict))
 
