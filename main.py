1c4b51e31564d6ab4191b71ec6789ccbb4eaa688
Aadam Mirza <87286325+aamirza0@users.noreply.github.com>
2021-07-30 18:22:28 +0000

    Added prediction and model evaluation functions

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 #import necessary libraries
 import pandas as pd 
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
 #import the correct data. To keep the results as consistent as possible between manual implementation and Scikit implementation, mouseID (target values, 0 being no exercise and 1 being exercise), will be a large array and not a read-in from a csv, unfortunately. The sequence of zeroes and ones to plug in to the mouseID array should be in the LDACodes.txt file. 
+print('bruh')
 data = pd.read_csv('LDAWeek9RawData.csv')
-import numpy as np
+
 #Copy/Paste the sequence of ones and zeroes from the LDACodes files that corresponnd to the week being used for analysis (in other words, the code corresponding to the name of the csv being read in line 5) 
 mouseID = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,]
-#apply the LDA now
-lda = LinearDiscriminantAnalysis(solver = 'svd')
-mouseLDA = lda.fit_transform(data,mouseID)
-np.savetxt('Week9results.txt',mouseLDA)
+#Split the dataset into training and testing data for the raw data and classes of observations respectively through the train_test_split method. Setting test size and train size to 0.2 and 0.8 respectively makes the training data consist of 80% of the dataset, while the other 20% is used as test data. 
+mouseTrain, mouseTest, classTrain, classTest = train_test_split(data,mouseID, test_size = 0.2, train_size = 0.8)
+#With the creation of the training data complete, create and fit an LDA object to it. 
+mouseLDA = LinearDiscriminantAnalysis()
+mouseLDA.fit(mouseTrain,classTrain)
+#Now that that's done, evaluate the LDA model using the test data from earlier, and compare results to determine model accuracy. 
+classPredict = mouseLDA.predict(mouseTest)
+print('Accuracy of Model: ', accuracy_score(classTest,classPredict))
 
-print(mouseLDA)
-# Explained variance ratio does not work, throws following error:"AttributeError: 'numpy.ndarray' object has no attribute 'explained_variance_ratio_'"
-##mouseLDA.explained_variance_ratio_
+
+
